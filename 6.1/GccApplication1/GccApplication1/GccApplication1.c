72808a65d00abd6ebd91dbe93de1e1acf40572f7
roney <roneymonte@gmail.com>
2013-11-11 08:58:25 -0200

    iniciando diretorio Atmel Studio 6.1

---
+++ 6.1/GccApplication1/GccApplication1/GccApplication1.c
@@ -0,0 +1,17 @@
+/*
+ * GccApplication1.c
+ *
+ * Created: 11/11/2013 08:50:04
+ *  Author: Roney
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
