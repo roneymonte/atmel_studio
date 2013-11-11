355d71cfcbb4244fa63429104efd10f1df687e8e
roney <roneymonte@gmail.com>
2013-11-11 07:50:00 -0800

    blink funcionando

--- 6.1/GccApplication1/GccApplication1/GccApplication1.c
+++ 6.1/GccApplication1/GccApplication1/GccApplication1.c
@@ -7,13 +7,34 @@
 
 
 #include <avr/io.h>
+#include <util/delay.h> 
+
+#define F_CPU 20000000
+
 
 int main(void)
 {
+	DDRC = 0xFF;
+	DDRB = 0xFF;
+	
+	PORTB = 0b0001;
+	
     while(1)
     {
-        //TODO:: Please write your application code 
+		PORTC |= (1<<0);
+		//PAUSE 250 miliseconds
+		PORTB = 0b0000;
+		_delay_ms(250);
 		
+		//turns C0 LOW
+		PORTC &= ~(1 << 0);
+		//PAUSE 250 miliseconds
+		PORTB = 0b0001;
+		_delay_ms(250);
 		
+		PORTC ^= _BV(PC0);
+		//_delay_ms(2500);
+
     }
 }
+
