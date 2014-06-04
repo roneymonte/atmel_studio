95bf63c296610432d2775a344a1c029ea94b59df
roney <roneymonte@users.noreply.github.com>
2014-06-03 23:17:43 -0300

    Projeto Inicial HelloWorld Blink

---
+++ 01_blinkings/01_blinkings/01_blinkings.c
@@ -0,0 +1,17 @@
+/*
+ * _01_blinkings.c
+ *
+ * Created: 03/06/2014 23:07:05
+ *  Author: roney
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
