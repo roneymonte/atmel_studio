a60604668afb60139d8e26805c3ee488306a2b9a
roney <roneymonte@users.noreply.github.com>
2014-06-30 15:53:04 -0300

    Tentativa de migracao de projeto Stand Alone para o ambiente Atmel Studio.

---
+++ 02_Avr8_LCD_via_AS/02_Avr8_LCD_via_AS/02_Avr8_LCD_via_AS.c
@@ -0,0 +1,17 @@
+/*
+ * _02_Avr8_LCD_via_AS.c
+ *
+ * Created: 30/06/2014 15:44:03
+ *  Author: roney
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
