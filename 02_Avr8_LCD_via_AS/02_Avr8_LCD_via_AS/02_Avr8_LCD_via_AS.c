f6b95acc2d8a9383876b67e5b6c7cdf0d8b7571b
roney <roneymonte@users.noreply.github.com>
2014-06-30 17:42:45 -0300

    atualizado com timer final

--- 02_Avr8_LCD_via_AS/02_Avr8_LCD_via_AS/02_Avr8_LCD_via_AS.c
+++ 02_Avr8_LCD_via_AS/02_Avr8_LCD_via_AS/02_Avr8_LCD_via_AS.c
@@ -6,18 +6,22 @@
  */ 
 
 #define  F_CPU 1000000UL 
+#define DELAY_MOVER 50
 
 #include <avr/io.h>
 #include "lcd.h"
 #include <util/delay.h>
+#include <stdlib.h>
+
+void direita8 (void);
+void esquerda8 (void);
 
 int main(void)
 {
-	
-	short int a;
-	
 	DDRB |= (1<<PORTB0);
-	lcd_init(LCD_DISP_ON_CURSOR_BLINK);
+	lcd_init(LCD_DISP_ON);
+	lcd_gotoxy(3,0); lcd_puts("by");
+	lcd_gotoxy(1,1); lcd_puts("Roney");
 	
     while(1)
     {
@@ -33,36 +37,58 @@ int main(void)
 		lcd_puts("AtmelS62");
 		_delay_ms(250);
 		
-		for (a=0;a<8;a++)
-		{
-		lcd_command(LCD_MOVE_DISP_LEFT);
-		_delay_ms(250);
-		}
+	//	lcd_command(LCD_DISP_ON_BLINK);
+	//	lcd_command(LCD_DISP_ON);
 		
-		for (a=0;a<8;a++)
-		{
-			lcd_command(LCD_MOVE_DISP_RIGHT);
-			_delay_ms(250);
-		}
-		_delay_ms(500);
-		for (a=0;a<8;a++)
-		{
-			lcd_command(LCD_MOVE_DISP_RIGHT);
-			_delay_ms(250);
-		}
-		_delay_ms(500);
-		
-		for (a=0;a<8;a++)
-		{
-			lcd_command(LCD_MOVE_DISP_LEFT);
-			_delay_ms(250);
-		}
+		esquerda8();
+		direita8();
+		direita8();
+		esquerda8();
 		
 		_delay_ms(1000);
 		
+		lcd_clrscr();
 			
+		lcd_gotoxy(1,1);
+		lcd_puts("Final");
 		
+		lcd_gotoxy(1,0);
+		lcd_puts("Demo");
+		
+		unsigned short int b;
+		char buf[2];
+		
+		for (b=21;b>0;b--)
+		{
+			lcd_gotoxy(6,0);
+			itoa(b-1,buf,10);
+			
+			lcd_puts(buf);
+			lcd_puts(" ");
+			_delay_ms(1000);
+		}
+		
+		_delay_ms(5000);
 		
         //TODO:: Please write your application code 
     }
+}
+
+void esquerda8 (void)
+{	unsigned short int a;
+	for (a=0;a<8;a++)
+	{
+		lcd_command(LCD_MOVE_DISP_LEFT);
+		_delay_ms(DELAY_MOVER);
+	}
+}
+
+void direita8 (void)
+{
+	unsigned short int a;
+	for (a=0;a<8;a++)
+	{
+		lcd_command(LCD_MOVE_DISP_RIGHT);
+		_delay_ms(DELAY_MOVER);
+	}
 }
