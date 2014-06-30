2a500c5e484affc9f11e0e26e01f07bbd55b87b4
roney <roneymonte@users.noreply.github.com>
2014-06-30 16:23:26 -0300

    Primeira versao interna de teste do AS62

--- 02_Avr8_LCD_via_AS/02_Avr8_LCD_via_AS/02_Avr8_LCD_via_AS.c
+++ 02_Avr8_LCD_via_AS/02_Avr8_LCD_via_AS/02_Avr8_LCD_via_AS.c
@@ -5,13 +5,26 @@
  *  Author: roney
  */ 
 
+#define  F_CPU 1000000UL 
 
 #include <avr/io.h>
+#include "lcd.h"
+#include <util/delay.h>
 
 int main(void)
 {
     while(1)
     {
+		lcd_clrscr();
+		
+		lcd_puts("AVR 168P\n");
+		lcd_puts("Atmel 62");
+		
+		_delay_ms(1000);
+		
+			
+		
+		
         //TODO:: Please write your application code 
     }
 }
