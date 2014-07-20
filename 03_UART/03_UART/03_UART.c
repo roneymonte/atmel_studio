42d39bc552ec028efdebd64c7afd3dd49c54471f
roney <roneymonte@users.noreply.github.com>
2014-07-20 19:34:37 -0300

    Projeto AVR com UART (versao iniciao)

---
+++ 03_UART/03_UART/03_UART.c
@@ -0,0 +1,17 @@
+/*
+ * _03_UART.c
+ *
+ * Created: 20/07/2014 19:29:01
+ *  Author: roney
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
