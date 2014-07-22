fbfeb0dbdfa5b9e8e1d1c1e9018cc0c101dc4b45
roney <roneymonte@users.noreply.github.com>
2014-07-22 16:20:03 -0300

    Atualizacao do Projeto com UART do Capitulo 5 do Livro "Make: AVR Programming".

--- 03_UART/03_UART/03_UART.c
+++ 03_UART/03_UART/03_UART.c
@@ -5,13 +5,31 @@
  *  Author: roney
  */ 
 
+#define F_CPU 1000000UL
 
 #include <avr/io.h>
+#include <util/delay.h>
+#include "pinDefines.h"
+#include "USART.h"
+
+
+
+
 
 int main(void)
 {
+	char serialCharacter;
+	
+	LED_DDR=0xff;
+	initUSART();
+	printString("Ola Mundo\r\n");
+	
     while(1)
     {
-        //TODO:: Please write your application code 
-    }
+		serialCharacter = receiveByte();
+		transmitByte(serialCharacter);
+		LED_PORT=serialCharacter;
+	}
+	
+	return(0);
 }
