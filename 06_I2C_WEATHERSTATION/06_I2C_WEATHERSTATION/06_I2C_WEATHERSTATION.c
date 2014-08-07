38784c10917fa709d6e0ddac777327f2ccb99b13
roney <roneymonte@users.noreply.github.com>
2014-08-07 09:12:49 -0300

    Criacao do Projeto 06 - I2C na Weather Station

---
+++ 06_I2C_WEATHERSTATION/06_I2C_WEATHERSTATION/06_I2C_WEATHERSTATION.c
@@ -0,0 +1,17 @@
+/*
+ * _06_I2C_WEATHERSTATION.c
+ *
+ * Created: 07/08/2014 09:10:20
+ *  Author: roney
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
